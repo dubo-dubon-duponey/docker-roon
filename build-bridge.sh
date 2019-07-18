ed8168ddca7ebde273c87d769ba7d7fbbfca7816
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-07-18 14:07:03 -0700

    Normalize images
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- build-bridge.sh
+++ build-bridge.sh
@@ -3,8 +3,9 @@
 IMAGE_OWNER="${IMAGE_OWNER:-dubodubonduponey}"
 IMAGE_NAME="${IMAGE_NAME:-roon-bridge}"
 IMAGE_VERSION="${IMAGE_VERSION:-v1}"
-PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64,linux/arm/v7}"
+PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64,linux/arm/v7}" # No v6
 
+export DOCKER_CONTENT_TRUST=1
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx create --name "$IMAGE_NAME"
 docker buildx use "$IMAGE_NAME"
--- build-server.sh
+++ build-server.sh
@@ -3,8 +3,9 @@
 IMAGE_OWNER="${IMAGE_OWNER:-dubodubonduponey}"
 IMAGE_NAME="${IMAGE_NAME:-roon-server}"
 IMAGE_VERSION="${IMAGE_VERSION:-v1}"
-PLATFORMS="${PLATFORMS:-linux/amd64}"
+PLATFORMS="${PLATFORMS:-linux/amd64}" # Just amd64
 
+export DOCKER_CONTENT_TRUST=1
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx create --name "$IMAGE_NAME"
 docker buildx use "$IMAGE_NAME"
