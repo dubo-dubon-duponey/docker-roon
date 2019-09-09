b5b271cb8f6e83efd0ab7b21eb44cdd6380743a9
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-09-09 15:42:58 -0700

    Minor cleanup
    - minor cleanup
    - refresh roon build
    - possibly fix bridge unique id persistence
    
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- refresh.sh
+++ refresh.sh
@@ -16,14 +16,14 @@ geturl(){
       url="armv7hf"
     ;;
   esac
-  echo "http://download.roonlabs.com/builds/RoonBridge_linux$url.tar.bz2"
+  printf "http://download.roonlabs.com/builds/RoonBridge_linux$url.tar.bz2"
 }
 
 for platform in linux/amd64 linux/arm64 linux/arm/v7; do
   mkdir -p ./cache/"$platform"
   if ! curl -fsSL -o ./cache/"$platform"/bridge.tar.bz2 "$(geturl "${platform}")"; then
     rm -f ./cache/"$platform"/bridge.tar.bz2
-    echo "Failed to download bits!"
+    >&2 printf "Failed to download bits!\n"
     exit 1
   fi
 done
@@ -31,6 +31,6 @@ done
 mkdir -p ./cache/linux/amd64
 if ! curl -fsSL -o ./cache/linux/amd64/server.tar.bz2 "http://download.roonlabs.com/builds/RoonServer_linuxx64.tar.bz2"; then
   rm -f ./cache/linux/amd64/server.tar.bz2
-  echo "Failed to download bits!"
+  >&2 printf "Failed to download bits!\n"
   exit 1
 fi
