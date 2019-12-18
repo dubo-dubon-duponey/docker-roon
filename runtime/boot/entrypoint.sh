130d237a5480fcaec7084e9c2473402970c78cfb
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-12-18 08:58:29 -0800

    Fix a bunch of issues
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- runtime/boot/entrypoint.sh
+++ runtime/boot/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 
-if [ -e /boot/bin/Server/RoonServer ]; then
+if [ -e /boot/bin/RoonServer/Server/RoonServer ]; then
   exec /boot/bin/RoonServer/Server/RoonServer
 else
   exec /boot/bin/RoonBridge/Bridge/RoonBridge
