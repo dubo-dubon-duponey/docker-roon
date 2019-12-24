b30f8b646abbfa34985b9c5c45a3f441175ee4a5
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2019-12-23 21:46:18 -0800

    Updated develop.md
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit -o errtrace -o functrace -o nounset -o pipefail
 export TITLE="Roon Bridge"
 export DESCRIPTION="A dubo image for Roon Bridge"
 export IMAGE_NAME="roon-bridge"
-export PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7" # No v6
+export PLATFORMS="${PLATFORMS:-linux/amd64,linux/arm64,linux/arm/v7}" # No v6
 
 # shellcheck source=/dev/null
 . "$(cd "$(dirname "${BASH_SOURCE[0]:-$PWD}")" 2>/dev/null 1>&2 && pwd)/helpers.sh" --target runtime-bridge
