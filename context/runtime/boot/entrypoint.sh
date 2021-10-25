f788b58526ed01e411e52e470923e9ef30e91df9
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-10-25 13:56:19 -0700

    Cosmetic
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- context/runtime/boot/entrypoint.sh
+++ context/runtime/boot/entrypoint.sh
@@ -46,9 +46,9 @@ helpers::dir::writable "$XDG_CACHE_HOME" create
 # mDNS blast if asked to
 [ ! "${MDNS_HOST:-}" ] || {
   _mdns_port="$([ "$TLS" != "" ] && printf "%s" "${PORT_HTTPS:-443}" || printf "%s" "${PORT_HTTP:-80}")"
-  [ ! "${MDNS_STATION:-}" ] || mdns::add "_workstation._tcp" "$MDNS_HOST" "${MDNS_NAME:-}" "$_mdns_port"
-  mdns::add "${MDNS_TYPE:-_http._tcp}" "$MDNS_HOST" "${MDNS_NAME:-}" "$_mdns_port"
-  mdns::start &
+  [ ! "${MDNS_STATION:-}" ] || mdns::records::add "_workstation._tcp" "$MDNS_HOST" "${MDNS_NAME:-}" "$_mdns_port"
+  mdns::records::add "${MDNS_TYPE:-_http._tcp}" "$MDNS_HOST" "${MDNS_NAME:-}" "$_mdns_port"
+  mdns::records::broadcast &
 }
 
 # Start the sidecar
